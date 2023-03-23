8b2b9b33d79771197af25d8aa4f931166acb3014
mick <alfred.scott@protonmail.com>
2023-03-22 22:56:35 -0400

    psuedo_code_v1

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# PSUEDO CODE
-
-# 1. read data from file into a list of lines
-# 2. iterate through the list and use a regex pattern that checks for an integer followed by a period and space
-# 3. if a new question begins in the line, split the line into two parts composed of the detected integer, and the
-#    question text string that follows
-# 4. before iterating to the next line use the integer, and question text to create a new Question() object
-# 5. append the new question to the list of questions
-# 6. if no integer matching the pattern is detected this indicates that the line contains question text - use the
 import re
 
 
@@ -22,13 +13,6 @@ class Question:
 
     def add_answer(self, answer):
         self.answers.append(answer)
-
-
-def q_num_in_q_text(index):
-    if index > 0:
-        return True
-    else:
-        return False
 
 
 q_num_pattern = re.compile(r'\d{1,3}\.\s')
@@ -60,21 +44,31 @@ answer_list = [
 ]
 
 with open('test_data_0') as f:
-    answr_started = False
     for line in f:
         q_num_bool = q_num_pattern.search(line)
         q_num_matches = q_num_pattern.finditer(line)
         answr_bool = answr_pattern.search(line)
         answr_matches = answr_pattern.finditer(line)
-        if q_num_bool:
-            for q in q_num_matches:
-                if q_num_in_q_text(q.start()):
-                    print(line, end='')
-                else:
-                    answr_started = False
-                    print(q.group()+line[q.end():len(line)-1])
-                    new_question = Question(int(q.group()[q.start():q.end()-2]), line[q.end():len(line)-1])
-                    question_list.append(new_question)
+        if q_num_bool & answr_bool:
+            # if q_num in text body
+            # concatenate_question_text
+            # elif q_num in answer body
+            # new question
+            # else
+            # new question
+            pass
+        elif q_num_bool:
+            # new question
+            pass
+        elif answr_bool:
+            # if two answers in one line
+            # add both answers
+            # else
+            # add answer
+            pass
+        else:
+            # concatenate_question_text
+            pass
 
 for q in question_list:
     print(q.question_num, end='. ')

00710690fb7c4c45b15e36df057f7a8233f5882c
mick <alfred.scott@protonmail.com>
2023-03-30 00:24:19 -0400

    file data string demarcated with '\n' before q_num

--- main_v2.py
+++ main_v2.py
@@ -2,42 +2,32 @@ import re
 
 q_num_pattern = re.compile(r'[0-9]{1,3}\. ')
 choice_a_pattern = re.compile(r'A\. ')
-index_list = []
-index_dict = {}
 
 
 with open('ch_1_data') as f:
-    data = f.read()
-    new = data.replace('\n', ' ')
-    new = new.replace('A. ', ' A. ').replace('  ', ' ')
-    new = new.replace('B. ', ' B. ').replace('  ', ' ')
-    new = new.replace('C. ', ' C. ').replace('  ', ' ')
-    new = new.replace('D. ', ' D. ').replace('  ', ' ')
-    new = new.replace(' A. ', '\nA. ')
-    new = new.replace(' B. ', '\nB. ')
-    new = new.replace(' C. ', '\nC. ')
-    new = new.replace(' D. ', '\nD. ')
-    # print(new)
-    # for char in new:
-    #     print(char, end='')
+    f_data = f.read()
+    f_data = f_data.replace('\n', ' ')
     ct = 1
-    q_num_bool = q_num_pattern.search(new)
-    q_num_matches = q_num_pattern.finditer(new)
-    choice_a_bool = choice_a_pattern.search(new)
-    choice_a_matches = choice_a_pattern.finditer(new)
+    q_num_bool = q_num_pattern.search(f_data)
+    q_num_matches = q_num_pattern.finditer(f_data)
+    choice_a_bool = choice_a_pattern.search(f_data)
+    choice_a_matches = choice_a_pattern.finditer(f_data)
     for match in q_num_matches:
         q_num = int(match.group()[0:len(match.group())-2])
         if ct == q_num:
-            print(q_num, end=' ')
-            print(ct)
             ct += 1
 
     num_of_qs = ct-1
     ct = 1
     print(num_of_qs)
+
     while ct <= num_of_qs:
-        q_num_str = str(ct) + '. '
-        new = new.replace(q_num_str, '\n'+q_num_str)
-        # print(q_num_str)
+        q_num_p = re.compile(rf'{str(ct)}\. ')
+        if not re.search(q_num_p, f_data):
+            print('No')
+        match = re.search(q_num_p, f_data)
+        start = match.start()
+        if match.group() != '1. ':
+            f_data = f_data[:int(match.start())]+'\n'+f_data[int(match.start()):]
         ct += 1
-    print(new)
+    print(f_data)
